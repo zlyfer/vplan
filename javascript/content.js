995e161c462d9711b91e1fd97c2c959f25ca342e
Frederik Shull <frederik.shull@web.de>
2018-09-07 12:33:12 +0200

    few fixes

--- javascript/content.js
+++ javascript/content.js
@@ -1,7 +1,7 @@
 function content_init() {
-  document.getElementById('suchen').addEventListener('click', async (e) => {
-    let kurs = document.getElementById('kurs').value;
-    let vertretungen = await api(kurs);
-    document.getElementById('test').value = JSON.stringify(vertretungen);
-  });
+	document.getElementById('suchen').addEventListener('click', async (e) => {
+		let kurs = document.getElementById('kurs').value;
+		let vertretungen = await api(kurs);
+		document.getElementById('test').value = JSON.stringify(vertretungen);
+	});
 }
